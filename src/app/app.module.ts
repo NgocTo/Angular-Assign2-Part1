715d37e04e6d01d4a90558aa1da5dbf4f9063bad
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-06-13 14:30:57 -0400

    imported form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -12,7 +12,8 @@ import { CustomerComponent } from './customer/customer.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
